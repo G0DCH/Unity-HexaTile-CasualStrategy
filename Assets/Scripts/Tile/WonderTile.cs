90b09111a230eea19df48d6f25ca959d9d3003b1
G0DCH <45649338+G0DCH@users.noreply.github.com>
2020-07-21 14:43:59 +0900

    New : 불가사의 추상 클래스 생성

---
+++ Assets/Scripts/Tile/WonderTile.cs
@@ -0,0 +1,16 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TilePuzzle
+{
+    // 불가사의 타일
+    public abstract class WonderTile : Tile
+    {
+        // 불가사의 건설 제한
+        public abstract bool WonderLimit();
+        // 불가사의 효과
+        public abstract void WonderFunction();
+    }
+}
