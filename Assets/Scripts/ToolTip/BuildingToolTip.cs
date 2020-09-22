303f4020eb92729abb945310bfa347e16d37ef74
G0DCH <45649338+G0DCH@users.noreply.github.com>
2020-09-22 16:38:50 +0900

    Add : 새로운 툴팁 테이블 용 스크립트 추가

---
+++ Assets/Scripts/ToolTip/BuildingToolTip.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+namespace TilePuzzle
+{
+    [SerializeField]
+    public class BuildingToolTip
+    {
+        /// <summary>
+        /// 이 툴팁의 건물
+        /// </summary>
+        public TileBuilding MyBuilding { get { return myBuilding; } }
+        [SerializeField]
+        private TileBuilding myBuilding = TileBuilding.Campus;
+
+        public string ToolTipText { get { return toolTipText; } }
+        [SerializeField, TextArea]
+        private string toolTipText;
+
+        public BonusPerAge MyBonusPerAge { get { return myBonusPerAge; } }
+        [SerializeField]
+        private BonusPerAge myBonusPerAge = new BonusPerAge();
+    }
+}
---
+++ Assets/Scripts/ToolTip/BuildingToolTipTable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+namespace TilePuzzle
+{
+    [CreateAssetMenu(menuName = "Tile Puzzle/ToolTipTable/BuildingToolTipTable")]
+    public class BuildingToolTipTable: ScriptableObject
+    {
+        public List<BuildingToolTip> BuildingToolTips { get { return buildingToolTips; } }
+        [SerializeField]
+        private List<BuildingToolTip> buildingToolTips = new List<BuildingToolTip>();
+    }
+}
