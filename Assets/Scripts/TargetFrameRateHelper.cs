88a0f8272cf3f035a24b809e8be5434b28c09301
Suwhan Kim <suwhan77@naver.com>
2020-12-16 23:47:57 +0900

    TargetFrameRateHelper 클래스 추가

---
+++ Assets/Scripts/TargetFrameRateHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TilePuzzle
+{
+    public class TargetFrameRateHelper : MonoBehaviour
+    {
+        [Range(15, 144)]
+        public int targetFrameRate = 60;
+
+        private void Start()
+        {
+            Application.targetFrameRate = targetFrameRate;
+        }
+    }
+}
