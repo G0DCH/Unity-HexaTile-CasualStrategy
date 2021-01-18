28b5737ff43e56162e8b0eb01c494a526ce36749
G0DCH <45649338+G0DCH@users.noreply.github.com>
2021-01-18 15:58:19 +0900

    Update : AI 개발 8 #12
    건물 설치 구현 완료

--- Assets/Scripts/Entity/AI/State/BuildingState.cs
+++ Assets/Scripts/Entity/AI/State/BuildingState.cs
@@ -1,4 +1,7 @@
-﻿namespace TilePuzzle.Entities.AI
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace TilePuzzle.Entities.AI
 {
     public class BuildingState : State
     {
@@ -15,8 +18,7 @@
             enemy.MyActionState = ActionState.Building;
         }
 
-        // 도시가 없다면 무작위 위치에 설치
-        // 있다면 기존 도시와 붙여서 설치
+        // 도시 범위 내 무작위 위치에 건물 설치
         public override void Excute(EnemyAI enemy)
         {
             if (!CheckEnemyTurn(enemy))
@@ -24,7 +26,19 @@
                 return;
             }
 
-            
+            HashSet<Tile> tiles = new HashSet<Tile>();
+
+            foreach (var ownCity in enemy.ownCitys)
+            {
+                tiles.UnionWith(ownCity.RangeTiles);
+            }
+
+            List<Tile> tileList = new List<Tile>(tiles);
+
+            PutRandomBuildingTile(tileList);
+
+            enemy.MyState = Idle.Instance;
+            // TODO : 다음 Entity에게 턴을 넘겨줘야 함.
         }
 
         public override void Exit(EnemyAI enemy)
@@ -35,6 +49,53 @@
             }
 
             StateMessage(false, enemy.NickName, nameof(BuildingState));
+        }
+
+        // 무작위 건물을 무작위 타일에 설치
+        private void PutRandomBuildingTile(List<Tile> tiles)
+        {
+            int enumCount = System.Enum.GetValues(typeof(TileBuilding)).Length;
+
+            // 설치하지 않을 건물들
+            HashSet<TileBuilding> ignoreBuildings = new HashSet<TileBuilding>
+                                { TileBuilding.City, TileBuilding.Empty, TileBuilding.Carnal, TileBuilding.Wonder };
+
+            TileBuilding randomBuilding;
+
+            while (true)
+            {
+                List<Tile> checkList = new List<Tile>(tiles);
+
+                // 무작위 빌딩 뽑기
+                while (true)
+                {
+                    randomBuilding = (TileBuilding)Random.Range(0, enumCount);
+
+                    if (!ignoreBuildings.Contains(randomBuilding))
+                    {
+                        break;
+                    }
+                }
+
+                while (true)
+                {
+                    var randomTile = TileManager.Instance.GetRandomEmptyTile(checkList);
+                    checkList.Remove(randomTile);
+
+                    // 설치 성공 시 함수 종료
+                    if (TileManager.Instance.PutBuildingAtTile(randomBuilding, randomTile))
+                    {
+                        return;
+                    }
+                    // 실패시 설치하지 않을 건물에 무작위 건물 추가하고
+                    // 다시 루프를 돔.
+                    else if (checkList.Count == 0)
+                    {
+                        ignoreBuildings.Add(randomBuilding);
+                        break;
+                    }
+                }
+            }
         }
     }
 }
